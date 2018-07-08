b212b1606d43bb72a649ac279bc1dadd8c85e4e9
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-07-08 12:27:30 +0200

    test to see if the console work: fine

--- scrape.js
+++ scrape.js
@@ -2,4 +2,6 @@
 
 // Look at https://github.com/HackYourFutureBelgium/JavaScript2/tree/master/Projects/web-scraper
 // for the version we showed in class.
+// to test and see if the console work
+console.log("hello world");
 
